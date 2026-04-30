72c2a9ae8f364eefcebfa7c5abd3fb2785cd1df8
origami.psc.br <origamis@gmail.com>
2026-04-29 21:58:06 -0300

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,41 +1,49 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
+// Layout compartilhado por todas as páginas
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [
+    Component.Flex({
+      components: [
+        {
+          Component: Component.PageTitle(),
+          grow: true,
+        },
+        { Component: Component.Search() },
+        { Component: Component.Darkmode() },
+      ],
+    }),
+  ],
   afterBody: [],
   footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/origami-terapia/quartz",
-    },
+    links: {},
   }),
 }
 
+// Páginas de conteúdo
 export const defaultContentPageLayout: PageLayout = {
+  beforeBody: [
+    Component.ConditionalRender({
+      component: Component.Breadcrumbs(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+    Component.TagList(),
+  ],
+  left: [],
+  right: [],
+}
+
+// Páginas de lista (tags, pastas, etc.)
+export const defaultListPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
   ],
   left: [],
-  right: [
-    Component.Search(),
-    Component.Explorer(),
-    Component.Graph(),
-    Component.TableOfContents(),
-    Component.Backlinks(),
-  ],
-}
-
-export const defaultListPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(),
-  ],
-  left: [],
-  right: [
-    Component.Search(),
-    Component.Explorer(),
-  ],
+  right: [],
 }
